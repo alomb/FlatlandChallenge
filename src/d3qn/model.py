c64830bf64753f58fe6b4d7a510fb4266f08d823
alessandro <alelombo55@gmail.com>
2020-08-30 17:43:08 +0200

    D3QN: change network names. Report: Extend D3QN.

--- src/d3qn/model.py
+++ src/d3qn/model.py
@@ -15,24 +15,24 @@ class DuelingQNetwork(nn.Module):
         super(DuelingQNetwork, self).__init__()
         self.shared = parameters.shared
         self.base_modules = nn.ModuleList([])
-        self.critic_modules = nn.ModuleList([])
-        self.actor_modules = nn.ModuleList([])
+        self.value_modules = nn.ModuleList([])
+        self.advantage_modules = nn.ModuleList([])
 
         if parameters.shared:
             self.base_modules.append(nn.Linear(state_size, parameters.hidden_size))
             for i in range(parameters.hidden_layers):
                 self.base_modules.append(nn.Linear(parameters.hidden_size, parameters.hidden_size))
         else:
-            self.critic_modules.append(nn.Linear(state_size, parameters.hidden_size))
-            self.actor_modules.append(nn.Linear(state_size, parameters.hidden_size))
+            self.value_modules.append(nn.Linear(state_size, parameters.hidden_size))
+            self.advantage_modules.append(nn.Linear(state_size, parameters.hidden_size))
 
             for i in range(parameters.hidden_layers):
-                self.critic_modules.append(nn.Linear(parameters.hidden_size, parameters.hidden_size))
-                self.actor_modules.append(nn.Linear(parameters.hidden_size, parameters.hidden_size))
+                self.value_modules.append(nn.Linear(parameters.hidden_size, parameters.hidden_size))
+                self.advantage_modules.append(nn.Linear(parameters.hidden_size, parameters.hidden_size))
 
         # Last layer is always separated
-        self.critic_modules.append(nn.Linear(parameters.hidden_size, 1))
-        self.actor_modules.append(nn.Linear(parameters.hidden_size, action_size))
+        self.value_modules.append(nn.Linear(parameters.hidden_size, 1))
+        self.advantage_modules.append(nn.Linear(parameters.hidden_size, action_size))
 
         loading = False
 
@@ -49,14 +49,14 @@ class DuelingQNetwork(nn.Module):
             val = F.relu(self.base_modules[i](val))
             adv = F.relu(self.base_modules[i](adv))
 
-        for i in range(len(self.critic_modules) - 1):
-            val = self.critic_modules[i](val)
+        for i in range(len(self.value_modules) - 1):
+            val = self.value_modules[i](val)
 
-        for i in range(len(self.actor_modules) - 1):
-            adv = self.actor_modules[i](adv)
+        for i in range(len(self.advantage_modules) - 1):
+            adv = self.advantage_modules[i](adv)
 
-        val = self.critic_modules[-1](val)
-        adv = self.actor_modules[-1](adv)
+        val = self.value_modules[-1](val)
+        adv = self.advantage_modules[-1](adv)
 
         return val + adv - adv.mean()
 
